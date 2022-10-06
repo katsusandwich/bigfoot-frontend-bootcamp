d068e85772216ad44f3a3f6f351480034f47958e
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-10-06 17:38:38 +0800

    form works but there is issue with axios post

--- src/Components/new.js
+++ src/Components/new.js
@@ -4,8 +4,7 @@ import logo from "../logo.png";
 import "../App.css";
 import { useForm } from "react-hook-form";
 import { BACKEND_URL } from "../constants.js";
-
-// class App extends React.Component {
+import { Navigate } from "react-router-dom";
 
 const NewSightingForm = () => {
   //useState to set every element of a new sightings post
@@ -21,9 +20,26 @@ const NewSightingForm = () => {
 
   // write a react form box thing using react hook forms
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
 
   // react form submit thing should do an axios post
+  const onSubmit = (data) => console.log(data);
+  axios
+    .post(`${BACKEND_URL}/sightings`, {
+      date,
+      location,
+      notes,
+    })
+    .then(() => {
+      // Clear form state
+      setDate("");
+      setLocation("");
+      setNotes("");
+      //navigate to home
+      Navigate(`/`);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   return (
     <div className="App">
@@ -31,14 +47,17 @@ const NewSightingForm = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("date")} />
-            <input {...register("location")} />
-            <input {...register("notes")} />
-            <select {...register("gender")}>
+            <input {...register("date")} type="datetime-local" />
+            <input {...register("location")} placeholder="Yishun, Singapore" />
+            <input
+              {...register("notes")}
+              placeholder="Big bear, bigger than human, walking around the park at night. Very scary."
+            />
+            {/* <select {...register("gender")}>
               <option value="female">female</option>
               <option value="male">male</option>
               <option value="other">other</option>
-            </select>
+            </select> */}
             <input type="submit" />
           </form>
         </div>
