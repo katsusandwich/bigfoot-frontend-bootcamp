f23d3ff031c56c60d059b6da9c9fe30081bdbea4
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-10-13 09:47:53 +0800

    form submit and form reset is successful yay

--- src/Components/new.js
+++ src/Components/new.js
@@ -2,27 +2,33 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import logo from "../logo.png";
 import "../App.css";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import { BACKEND_URL } from "../constants.js";
-import { Navigate } from "react-router-dom";
 
 const NewSightingForm = () => {
   //useState to set every element of a new sightings post
 
-  ////date
-  const [date, setDate] = useState("");
+  // ////date
+  // const [date, setDate] = useState("");
 
-  ///location
-  const [location, setLocation] = useState("");
+  // ///location
+  // const [location, setLocation] = useState("");
 
-  ///notes
-  const [notes, setNotes] = useState("");
+  // ///notes
+  // const [notes, setNotes] = useState("");
 
   // write a react form box thing using react hook forms
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState,
+    formState: { isSubmitSuccessful },
+  } = useForm();
 
   // react form submit thing should do an axios post
-  const onSubmit = (data) =>
+  const onSubmit = (data) => {
+    const { date, location, notes } = data;
     axios
       .post(`${BACKEND_URL}/sightings`, {
         date,
@@ -31,15 +37,21 @@ const NewSightingForm = () => {
       })
       .then(() => {
         // Clear form state
-        setDate("");
-        setLocation("");
-        setNotes("");
+        // setDate("");
+        // setLocation("");
+        // setNotes("");
         //navigate to home
-        Navigate(`/`);
       })
       .catch(function (error) {
         console.log(error);
       });
+  };
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset({ date: "", location: "", notes: "" });
+    }
+  });
 
   return (
     <div className="App">
